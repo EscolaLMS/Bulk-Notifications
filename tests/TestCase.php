c034f00096cd43f3ade7316294debe7bfb5441b8
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2024-02-20 15:47:15 +0100

    Init package

---
+++ tests/TestCase.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace EscolaLms\BulkNotifications\Tests;
+
+use EscolaLms\Auth\EscolaLmsAuthServiceProvider;
+use EscolaLms\Auth\Models\User;
+use EscolaLms\Bookmarks\EscolaLmsBookmarksServiceProvider;
+use EscolaLms\Core\Tests\TestCase as CoreTestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Laravel\Passport\PassportServiceProvider;
+use Spatie\Permission\PermissionServiceProvider;
+
+class TestCase extends CoreTestCase
+{
+    use DatabaseTransactions;
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ...parent::getPackageProviders($app),
+            PassportServiceProvider::class,
+            PermissionServiceProvider::class,
+            EscolaLmsAuthServiceProvider::class,
+            EscolaLmsBookmarksServiceProvider::class
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('auth.providers.users.model', User::class);
+        $app['config']->set('passport.client_uuids', true);
+    }
+}
