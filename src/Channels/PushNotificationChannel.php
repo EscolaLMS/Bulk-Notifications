525deafdc83e1053a3ae4ef54396a456c01c35fd
Tomasz Smolarek <59400506+dyfero@users.noreply.github.com>
2024-02-27 15:42:32 +0100

    Add administrable settings (#2)
    Co-authored-by: Tomasz Smolarek <tomasz.smolarek@escolasoft.com>

--- src/Channels/PushNotificationChannel.php
+++ src/Channels/PushNotificationChannel.php
@@ -2,21 +2,27 @@
 
 namespace EscolaLms\BulkNotifications\Channels;
 
+use EscolaLms\BulkNotifications\EscolaLmsBulkNotificationsServiceProvider;
 use EscolaLms\BulkNotifications\Exceptions\UnsupportedNotification;
 use EscolaLms\BulkNotifications\ValueObjects\Notification;
 use EscolaLms\BulkNotifications\ValueObjects\PushNotification;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
 use Kreait\Firebase\Contract\Messaging;
 use Kreait\Firebase\Exception\FirebaseException;
 use Kreait\Firebase\Exception\MessagingException;
+use Kreait\Firebase\Factory;
 use Kreait\Firebase\Messaging\CloudMessage;
 
 class PushNotificationChannel implements NotificationChannel
 {
-    public function __construct(
-        private Messaging $messaging
-    )
+    private Messaging $messaging;
+
+    public function __construct()
     {
+        $this->messaging = (new Factory())
+            ->withServiceAccount($this->getServiceAccount())
+            ->createMessaging();
     }
 
     /**
@@ -40,6 +46,11 @@ class PushNotificationChannel implements NotificationChannel
         }
 
         return CloudMessage::fromArray($notification->toPayload());
+    }
+
+    private function getServiceAccount(): array
+    {
+        return json_decode(Config::get(EscolaLmsBulkNotificationsServiceProvider::CONFIG_KEY . '.push.service_account'), true) ?? [];
     }
 
     public static function sections(): Collection
